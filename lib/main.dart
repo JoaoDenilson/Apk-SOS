3372467b4d18bc3b2f633bb4bc90b10ee2540b5d
Ramon Batista <50596238+ramon0306@users.noreply.github.com>
2020-03-05 09:55:34 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,34 @@ class _GetLocationPageState extends State<GetLocationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            userLocation == null
+                ? CircularProgressIndicator()
+                : Text("Location:" +
+                    userLocation["latitude"].toString() +
+                    " " +
+                    userLocation["longitude"].toString()),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  _getLocation().then((value) {
+                    setState(() {
+                      userLocation = value;
+                    });
+                  });
+                },
+                color: Colors.blue,
+                child: Text("Get Location", style: TextStyle(color: Colors.white),),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
  Future<Map<String, double>> _getLocation() async {
